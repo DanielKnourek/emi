3fcfc3d466c9a43d68aca880b5c1eb3d40591a9c
DanielKnourek <dev.danielknourek@gmail.com>
2025-10-30 01:21:27 +0100

    refactor: move share message to client

--- xplat/src/main/java/dev/emi/emi/network/CommandS2CPacket.java
+++ xplat/src/main/java/dev/emi/emi/network/CommandS2CPacket.java
@@ -5,24 +5,37 @@ import dev.emi.emi.api.recipe.EmiRecipe;
 import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.bom.BoM;
 import dev.emi.emi.registry.EmiCommands;
+import dev.emi.emi.registry.EmiShareRecipe;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.Identifier;
 
 public class CommandS2CPacket implements EmiPacket {
-	private final byte type;
-	private final Identifier id;
+    private final byte type;
+    private final Identifier id;
+    private String extraInfo;
 
-	public CommandS2CPacket(byte type, Identifier id) {
-		this.type = type;
-		this.id = id;
-	}
+    public CommandS2CPacket(byte type, Identifier id) {
+        this.type = type;
+        this.id = id;
+    }
+
+    public CommandS2CPacket(byte type, Identifier id, String extraInfo){
+        this.type = type;
+        this.id = id;
+        this.extraInfo = extraInfo;
+
+    }
 
 	public CommandS2CPacket(PacketByteBuf buf) {
 		type = buf.readByte();
 		if (type == EmiCommands.VIEW_RECIPE || type == EmiCommands.TREE_GOAL || type == EmiCommands.TREE_RESOLUTION) {
 			id = buf.readIdentifier();
-		} else {
+		} else if (type == EmiCommands.SHARE_RECIPE) {
+            id = buf.readIdentifier();
+            extraInfo = buf.readString();
+
+        } else {
 			id = null;
 		}
 	}
@@ -32,35 +45,40 @@ public class CommandS2CPacket implements EmiPacket {
 		buf.writeByte(type);
 		if (type == EmiCommands.VIEW_RECIPE || type == EmiCommands.TREE_GOAL || type == EmiCommands.TREE_RESOLUTION) {
 			buf.writeIdentifier(id);
-		}
-	}
+        } else if (type == EmiCommands.SHARE_RECIPE) {
+            buf.writeIdentifier(id);
+            buf.writeString(this.extraInfo);
+        }
+    }
 
-	@Override
-	public void apply(PlayerEntity player) {
-		if (type == EmiCommands.VIEW_RECIPE) {
-			EmiRecipe recipe = EmiApi.getRecipeManager().getRecipe(id);
-			if (recipe != null) {
-				EmiApi.displayRecipe(recipe);
-			}
-		} else if (type == EmiCommands.VIEW_TREE) {
-			EmiApi.viewRecipeTree();
-		} else if (type == EmiCommands.TREE_GOAL) {
-			EmiRecipe recipe = EmiApi.getRecipeManager().getRecipe(id);
-			if (recipe != null) {
-				BoM.setGoal(recipe);
-			}
-		} else if (type == EmiCommands.TREE_RESOLUTION) {
-			EmiRecipe recipe = EmiApi.getRecipeManager().getRecipe(id);
-			if (recipe != null && BoM.tree != null) {
-				for (EmiStack stack : recipe.getOutputs()) {
-					BoM.tree.addResolution(stack, recipe);
-				}
-			}
-		}
-	}
+    @Override
+    public void apply(PlayerEntity player) {
+        if (type == EmiCommands.VIEW_RECIPE) {
+            EmiRecipe recipe = EmiApi.getRecipeManager().getRecipe(id);
+            if (recipe != null) {
+                EmiApi.displayRecipe(recipe);
+            }
+        } else if (type == EmiCommands.VIEW_TREE) {
+            EmiApi.viewRecipeTree();
+        } else if (type == EmiCommands.TREE_GOAL) {
+            EmiRecipe recipe = EmiApi.getRecipeManager().getRecipe(id);
+            if (recipe != null) {
+                BoM.setGoal(recipe);
+            }
+        } else if (type == EmiCommands.TREE_RESOLUTION) {
+            EmiRecipe recipe = EmiApi.getRecipeManager().getRecipe(id);
+            if (recipe != null && BoM.tree != null) {
+                for (EmiStack stack : recipe.getOutputs()) {
+                    BoM.tree.addResolution(stack, recipe);
+                }
+            }
+        } else if (type == EmiCommands.SHARE_RECIPE) {
+            EmiShareRecipe.shareRecipe(player, id, extraInfo);
+        }
+    }
 
-	@Override
-	public Identifier getId() {
-		return EmiNetwork.COMMAND;
-	}
+    @Override
+    public Identifier getId() {
+        return EmiNetwork.COMMAND;
+    }
 }
