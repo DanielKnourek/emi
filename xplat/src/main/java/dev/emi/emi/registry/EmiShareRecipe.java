47f040a99e2db374a02f702b48cd35a1df9107e7
DanielKnourek <dev.danielknourek@gmail.com>
2025-10-30 01:21:27 +0100

    refactor: move share message to client
    # Conflicts:
    #	xplat/src/main/java/dev/emi/emi/network/CommandS2CPacket.java

---
+++ xplat/src/main/java/dev/emi/emi/registry/EmiShareRecipe.java
@@ -0,0 +1,53 @@
+package dev.emi.emi.registry;
+
+import dev.emi.emi.api.EmiApi;
+import dev.emi.emi.api.recipe.EmiRecipe;
+import dev.emi.emi.runtime.EmiLog;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
+
+
+public class EmiShareRecipe {
+
+    private static Identifier lastSharedRecipe = null;
+
+    public static void shareRecipe(PlayerEntity player, Identifier id, String senderDisplayName) {
+        // Command is received 2x on a client, allow only first occurrence of this command
+        // also works as primitive spam protection
+        if (lastSharedRecipe != null && lastSharedRecipe.equals(id)) {
+            return;
+        }
+        lastSharedRecipe = id;
+
+        EmiRecipe recipe = EmiApi.getRecipeManager().getRecipe(id);
+        if (recipe == null) {
+            EmiLog.info("Could not create sharing message");
+            return;
+        }
+
+        //TODO: change to Text.translatable
+        String itemDisplayName = "View Recipe"; // fallback display text
+        if (!recipe.getOutputs().isEmpty()){
+            recipe.getOutputs().get(0).getItemStack().getItem().getName().getString();
+        }
+        MutableText clickableId = Text.literal(String.format("[%s]", itemDisplayName));
+        Style style = Style.EMPTY
+                .withColor(Formatting.UNDERLINE)
+                .withColor(Formatting.AQUA)
+                .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/emi view recipe " + id))
+                //TODO: change to Text.translatable
+                .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Click to view recipe")));
+        clickableId.setStyle(style);
+
+        Text message = Text.translatable("chat.emi.recipe_share", senderDisplayName, clickableId);
+
+        player.sendMessage(message, false);
+    }
+
+}
